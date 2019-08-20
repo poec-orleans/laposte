1e8502b095cd7f50492cad1dcbbd61ee72e260b6
poec-orleans <Administrateur@10.210.15.64>
2019-08-20 12:01:45 +0200

    Test

---
+++ LAPOSTE/src/test/java/Test_1.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_1 {
+
+	@Test
+	public void test0() {
+		assertEquals(2, 2);
+	}
+	@Test
+	public void test1() {
+		assertEquals(2, 2);
+	}
+	@Test
+	public void test2() {
+		assertEquals(2, 2);
+	}
+
+}
